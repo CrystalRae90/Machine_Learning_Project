c56a1475674f57222f4b64760eb22b5ef2d97adb
Jay-ni13 <jennifer.cockerill13@gmail.com>
2023-02-13 20:52:22 -0600

    Updating Flask file

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, jsonify
+import tensorflow as tf
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
 	return render_template("home.html")
+
+# Load the saved model
+#model = tf.saved_model.load("/content/drive/My Drive/bird_model.h5")
+
+# @app.route("/predict", methods=["POST"])
+# #def predict():
+#     # Get the input data from the request
+#     input_data = request.get_json()
+
+#     # Use the model to make a prediction
+#     prediction = model.predict(input_data)
+
+#     # Return the prediction as a JSON response
+#     return jsonify({"prediction": prediction.tolist()})
+
 
 @app.route("/about")
 def about():
